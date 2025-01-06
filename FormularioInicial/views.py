e3a90e0f9e9d09ead6e9c9d12b9b438f84b2e28a
kotonitro <kotonitro554@gmail.com>
2025-01-06 14:08:21 -0300

    pipodiel

--- FormularioInicial/views.py
+++ FormularioInicial/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def FormularioInicial(request):
     return render(request, 'FormularioInicial.html')
+def CuerpoHumano(request):
+    return render(request, 'CuerpoHumano.html')
