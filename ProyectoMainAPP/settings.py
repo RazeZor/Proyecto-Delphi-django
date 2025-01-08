45274a988542cb7cb892beb554d697671788a02b
RazeZor <nachojaviert@gmail.com>
2025-01-08 09:23:24 -0300

    commit

--- ProyectoMainAPP/settings.py
+++ ProyectoMainAPP/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = 'ProyectoMainAPP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Clinica_Delphi',
+        'USER': 'root',
+        'PASSWORD': 'Maxitotaker83.',
+        'HOST': 'localhost',  
+        'PORT': '3306',       
     }
 }
+
+
 
 
 # Password validation
@@ -125,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
